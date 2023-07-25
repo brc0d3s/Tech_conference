671bc315180d8844aca8ea76e13cb69506846df8
brc0d3s <brc0d3s@gmail.com>
2023-07-25 17:07:08 +0300

    Tech

--- myapp/views.py
+++ myapp/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from .models import Room
 
+# rooms = [
+#     {'id': 1, 'name': 'Let\'s learn Python!', },
+#     {'id': 2, 'name': 'Design with me', },
+#     {'id': 3, 'name': 'Frontend developers', },
+# ]
 
-rooms=[
-    {'id':1,'name':'Let\'s learn Python!',},
-    {'id':2,'name':'Design with me',},
-    {'id':3,'name':'Frontend developers',},
-]
 
 def home(request):
-    context={'rooms':rooms}
-    return render(request,'myapp/home.html',context)
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'myapp/home.html', context)
 
 
+def room(request, pk):
+    room =Room.objects.get(id=pk)
+    context = context = {'room': room}
 
-def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context=context={'room':room}
-        
-    return render(request,'myapp/room.html',context)
+    return render(request, 'myapp/room.html', context)
 
+def createRoom(request):
+    context={}
+    return render(request,'myapp/room_form.html',context)
